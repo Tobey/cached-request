bc84318205753f791d23674bb95a70a88a21360c
Tobey <tobey@ueni.com>
2017-12-04 17:01:53 +0000

    bug fix

--- ueniapi/session.py
+++ ueniapi/session.py
@@ -21,15 +21,18 @@ class HTTPSession(requests.Session):
 
         if cache_request:
             requests_cache.install_cache()
+        if not request_uri:
+            raise UeniAPIError('request_uri is required')
 
-        trailing_slash = request_uri.endswith("/")
-        self.request_uri = f'{request_uri}{"/" if  not trailing_slash else ""}'
+        if not request_uri.endswith("/"):
+            request_uri = f'{request_uri}/'
+        self.request_uri = request_uri
         self.headers = {
             f'{auth_header}': f'{auth_prefix}{token}',
             'User-Agent': user_agent,
         }
 
-    def request(self, method, endpoint, params=None, data=None, **kwargs):
+    def request(self, method, endpoint, params=None, data=None, raise_for_status=True, **kwargs):
         headers = kwargs.pop('headers', {})
         headers.update(self.headers)
         response = super().request(
@@ -40,6 +43,8 @@ class HTTPSession(requests.Session):
             headers=headers,
             **kwargs
         )
+        if not raise_for_status:
+            return response
 
         try:
             response.raise_for_status()
